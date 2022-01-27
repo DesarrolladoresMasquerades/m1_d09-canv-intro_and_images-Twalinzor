95da9080670a10d13656a7167176fe7e0827033a
Stan Tapchylka <stanislautapchylka@gmail.com>
2022-01-27 13:42:28 +0100

    .

---
+++ 03-canv-energy-ball/scripts/fireball.js
@@ -0,0 +1,13 @@
+console.log("fireball loaded")
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const fireball = new Image()
+fireball.src = "images/fireBall.png"
+
+ctx.drawImage(
+    fireball,
+    0,0,
+    150, 100
+)
